69c13ebb8795f76f1b2358e81a83aa0469b78c80
pius <pius.opoku@pakatron.com>
2023-01-05 12:46:53 +0000

    solid

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,17 @@ use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Models\Permission;
 
+Route::group(['middleware' => 'guest'], function () {
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// Hide register after Admin account is created
+    Route::get('login', function () {
+        return view('auth.login');
+    })->name('login');
 
-Route::get('login', function () {
-    return view('auth.login');
-})->name('login');
+    Route::get('register', function () {
+        return view('auth.register');
+    })->name('register');
+});
 
-Route::get('register', function () {
-    return view('auth.register');
-})->name('register');
 
 Route::middleware([
     'auth:sanctum',
@@ -43,18 +34,17 @@ Route::middleware([
         return view('invoice');
     });
 
-    Route::get('manage-roles', function() {
+    Route::get('manage-roles', function () {
         $users = User::all();
         return view('manage-roles', compact('users'));
     });
-    Route::get('staff', function() {
+    Route::get('staff', function () {
         $users = User::all();
         return view('staff.index', compact('users'));
     })->name('staff');
 
     Route::view('/', 'welcome');
-    // Route::view('manage-roles', 'manage-roles', ['users' => User::all()]);
-    // Route::view('staff', 'staff.index', ['users' => User::all()])->name('staff');
+
     Route::view('documentation', 'documentation');
     Route::view('logs', 'logs');
 
